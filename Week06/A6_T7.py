755e4f93e5f8a23952d1095e37ddd4d8a8f29c63
Pauli Nieminen <paukenieminen@msn.com>
2025-12-16 21:35:13 +0200

    Week06

---
+++ Week06/A6_T7.py
@@ -0,0 +1,12 @@
+#Galba,Otho,Vitellius, Vespanian
+# travel with program
+
+# travel once per program run
+# start show current location followed by process of traveling to next 
+# strat location is Home or Start 
+# 0 Home
+# 1. Galbas's palace
+# 2. Otho's palace
+# 3. Vitellius' palace
+# 4. Vespansian's palace
+
